8590c6d9e6214b2ac1d2204120eca98f97b9c465
jimmy-oss <jimmynjonge51@gmail.com>
2024-06-24 13:27:16 +0300

    python variables

---
+++ variables.py
@@ -0,0 +1,11 @@
+# ------STRING-------#
+my_story = "Tumbo lisiloshiba"
+print(my_story)
+
+# ----------NUMBERS------#
+my_number = 32
+print(my_number)
+
+# ----BOOLEAN------#
+am_i_young = True
+print(am_i_young)
