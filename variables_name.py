8a53729d21d367f60280e91237191f482ae98684
jimmy-oss <jimmynjonge51@gmail.com>
2024-06-24 14:00:59 +0300

    variables_rules

---
+++ variables_name.py
@@ -0,0 +1,20 @@
+# -------Variable Rules--------#
+
+# 1.Variable names must make sense
+movie_rating = 8.5
+
+# 2.Python convention is to use lowercase letters
+my_name = "jimmy"
+
+# 3.Python is snake case
+# myName # Camel case used in javascript
+user_info = "Online"
+
+# 4.Variable names must start with a letter or _
+# 25street # Error
+
+# 5.Following the pep8 standards
+# When your saving your python3 file the pep8 is
+# going to indent your codes and make your codes appealing
+# and professional
+to_do = "buy a beer"
