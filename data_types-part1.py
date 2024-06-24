15f3bdd81dbc1980f15675ce49ef9a2f822f5ab3
jimmy-oss <jimmynjonge51@gmail.com>
2024-06-24 14:32:07 +0300

    data-types

---
+++ data_types-part1.py
@@ -0,0 +1,36 @@
+# Data Types part 1
+
+"""
+--Data Type--         --Class--                 --Value--
+integers               -> int                     -> 45
+floating point numbers -> float                   -> 4.5
+booleans               -> bool                    -> True/False
+strings                -> str                     -> "Jimmy"
+list                   -> list                    -> [1, 2, 3]
+dictionary             -> dict                    -> {"user_name": "awesome50", "user_id": 56}
+tuple                  -> tuple                   -> (10, 20, 30)
+set                    -> set                     -> {"cat", 99}
+"""
+
+# Numbers
+# The type of age is an integer
+age = 57
+print(age)
+print(type(age))
+
+# Floats
+grade = 8.9
+print(grade)
+print(type(grade))
+
+# Booleans
+alarm = True
+offline = False
+print(alarm, offline)
+print(type(alarm))
+print(type(offline))
+
+# Strings
+my_name = "Jimmy"
+print(my_name)
+print(type(my_name))
