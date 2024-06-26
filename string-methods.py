ace56cfc218ee91e5b88a96be9c9e464d2b601f4
jimmy-oss <jimmynjonge51@gmail.com>
2024-06-26 13:32:16 +0300

    string methods

---
+++ string-methods.py
@@ -0,0 +1,16 @@
+# ---Strings ---
+# 1- Len() it tells us how many characters we have
+address = "Kenya"
+print(len(address))
+
+# 2- [] Notation used to access a certain character on a string
+print(address[0])
+print(address[4])
+print(address[-2])
+
+# 3- [] Notation can also be used at the bracket to extract specific numbers on a string
+print(address[0:3])
+print(address[-3:-1])
+print(address[0:])
+print(address[:-1])
+print(address[:])
