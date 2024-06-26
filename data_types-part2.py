237231e7291fabf1388995716c65d5055011f170
jimmy-oss <jimmynjonge51@gmail.com>
2024-06-26 13:08:39 +0300

    data-types2

---
+++ data_types-part2.py
@@ -0,0 +1,37 @@
+# Data Types part 2
+
+"""
+--Data Type--         --Class--                 --Value--
+integers               -> int                     -> 45
+floating point numbers -> float                   -> 4.5
+booleans               -> bool                    -> True/False
+strings                -> str                     -> "Jimmy"
+list                   -> list                    -> [1, 2, 3]
+dictionary             -> dict                    -> {"user_name": "awesome50", "user_id": 56}
+tuple                  -> tuple                   -> (10, 20, 30)
+set                    -> set                     -> {"cat", 99}
+"""
+# -----Lists-----
+# Ordered
+mixed = [1,2,3,4.6,2.5,True,[1,2,3,4],"jimmy"]
+#print(mixed)
+#print(type(mixed))
+ 
+# ---Dictionary----
+# Unordered
+user_info= {"user_name": "awesome50", "user_id": 56}
+#print(user_info)
+#print(type(user_info))
+
+# ---Tuple----
+# Ordered
+mixed_tuple = (1,2,3,4.6,2.5,True,[1,2,3,4],"jimmy")
+#print(mixed_tuple)
+#print(type(mixed_tuple))
+
+# ---Sets---
+# Unordered
+mixed_set = {1,2,3.2,5.4,"Python","jimmy",101}
+print(mixed_set)
+print(type(mixed_set))
+
