71b6dcf27494a7a422642b49dd77d764421cabd3
jimmy-oss <jimmynjonge51@gmail.com>
2024-07-05 15:59:04 +0300

    empty_conditions

---
+++ nothing.py
@@ -0,0 +1,16 @@
+# Checking for nothing
+
+name = ""
+list_a = []
+
+if list_a :
+  print("I will not run")
+else:
+  print("I am empty")
+  
+# So in short if we create a variable and pass
+# in an empty condition our python interpretor 
+# considers our variables which is empty as false
+# The if statement runs if our output condition is
+# True and since our list is empty our condition is
+# considered to be false
