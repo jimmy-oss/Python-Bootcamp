8323a34d59ee51015caa80835452794b511c6264
jimmy-oss <jimmynjonge51@gmail.com>
2024-07-05 15:31:21 +0300

    control_flows

---
+++ comparison_operators.py
@@ -0,0 +1,14 @@
+# Comparison Operators
+
+# Let us look at some of the comparison operators
+# we will be using in Python
+
+# > Greater than
+# < Less than
+# ==Equal to
+# !=not equal to
+# >= Greater than or equal to
+# <=Less than or Equal to
+# and Checks if both conditions evaluate to true
+# or Checks if at least one condition is true
+# not returns the opposite of the condition given
---
+++ control_flow.py
@@ -0,0 +1,9 @@
+# if statements
+height = 74 # The unit is inches
+
+if height > 70 :
+  print("You're really tall")
+  
+# Since height is above 70, 
+# the if statement will be called and it will print out
+# the statement
