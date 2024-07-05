19321ae3e086caa50346f90354749dd7145e4c8d
jimmy-oss <jimmynjonge51@gmail.com>
2024-07-05 16:29:51 +0300

    the from method

---
+++ from_method.py
@@ -0,0 +1,15 @@
+# From Keyword
+# The from keyword is used to import a module
+from math import sqrt,ceil
+
+x = sqrt(9)
+print(x)
+
+y = ceil(x)
+print(y)
+
+# We were able to use sqrt() and ciel 
+# without typing the import math module
+# When we use the from keyword we can use
+# the method name directly without referencing
+# the module
