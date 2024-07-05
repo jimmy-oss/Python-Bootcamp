ac90b6b61a0877a18304f24718eb131e1ec7c7d2
jimmy-oss <jimmynjonge51@gmail.com>
2024-07-05 13:28:51 +0300

    the sys_module

---
+++ sys_module.py
@@ -0,0 +1,13 @@
+import sys
+
+name = sys.argv[1]
+print("How old are you?")
+age = int(input())
+
+print(name)
+print(age)
+
+# Since sys.argv returns a list we used the sys.argv[1]
+# This simply tells our application to pick the argument
+# that is at index 1 in the list.
+# the application is run and stores it as a variable name
