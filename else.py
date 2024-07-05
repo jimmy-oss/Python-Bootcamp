dfc6e22b8b614b7d62b64ae3ff9b5a941e6b13ab
jimmy-oss <jimmynjonge51@gmail.com>
2024-07-05 15:36:28 +0300

    else

---
+++ else.py
@@ -0,0 +1,14 @@
+# Else
+
+# The else statement comes right at the end of if statement
+# It is used to execute code if the if statement is false
+
+height = 54 #inches
+if height > 70 :
+  print("You're really tall")
+else:
+  print("You're not really tall")
+  
+# The height is not greater than 70 
+# so the if statement evaluates to False
+# and the else statement is called
