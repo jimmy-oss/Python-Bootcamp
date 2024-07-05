fc4b2ac9b6e20bdc3d071c5007d0d6683b6974f6
jimmy-oss <jimmynjonge51@gmail.com>
2024-07-05 21:20:13 +0300

    loop conditions

---
+++ loop_conditions.py
@@ -0,0 +1,20 @@
+# for Loop conditions
+numbers = [1,2,3,4,5]
+for i in numbers:
+  if i % 2 == 0:
+    print(i)
+    
+# The purpose of the code is achieved
+# through the use of a for loop and an
+# if statement.The for loop iterates over
+# each element i in the numbers list.
+# For each iteration,the if statement checks
+# if the value of i is even by using the modulus
+# operator % .If the remainder of i divided by 2 is 0
+#. It means that i is an even number.
+# if the condition i % 2 == 0 is true the
+# if block is executed which is simply print(i)
+# This line of code prints the value of i to the console
+# After the loop completes, the program will have printed
+# all the even numbers from the numbers list which was provided
+# to the console.
