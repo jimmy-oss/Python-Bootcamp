ab1e9df834b1822c52e8a3618587221e942b13f2
jimmy-oss <jimmynjonge51@gmail.com>
2024-07-05 16:23:55 +0300

    module_method

---
+++ import.py
@@ -0,0 +1,27 @@
+# Modules and Packages in python3
+# Modules are files containing Python definitions and statements.
+# A module can define functions, classes, and variables
+# Package is made up of one or more modules that are
+# joined to perform a particular task
+
+# Math Module
+import math
+b = math.sqrt(9)
+print(b)
+
+y = math.ceil(b)
+print(y)
+
+# In python3 there are many import out there but
+# the most popular one is called the math module
+# which is used to perform mathematical operations
+# on numbers
+# The sqrt() method is used to find the square root of 9 
+# which is 3.0 is represented on our variable b
+# The ceil() method is used to find the nearest integer
+# the method rounds up our b = (3) into 3.0
+# The Syntax that is used in module methods
+# module_name is the name of the module in our case our module
+# is import math then there is
+# .method_name is the name of the method which in our case
+# we have used .sqrt() and .ciel()
