6aa580d4f59e1cdbbc2f7275469f07ff6b81effa
jimmy-oss <jimmynjonge51@gmail.com>
2024-07-05 16:50:28 +0300

    random_method

---
+++ random_method.py
@@ -0,0 +1,16 @@
+# Random Module
+# The random module is used to generate random numbers
+
+import random
+# We can generate a random number using the randint method
+
+random_number = random.randint(0,10)
+
+print(random_number)
+
+# From the module we used the randint() method
+# and passed in two parameters to define the range
+# of which python3 will choose a random number 
+# between 0 and 10
+# When we run the file it returns a random number from the
+# range we provided
