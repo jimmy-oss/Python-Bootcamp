b9d0ea017e3870760a4a31e13701f82da762b692
jimmy-oss <jimmynjonge51@gmail.com>
2024-07-06 15:11:08 +0300

    tuples

---
+++ tuples.py
@@ -0,0 +1,15 @@
+# Tuples
+# Tuples are like lists the main difference
+# between tuples and lists is that tuples
+# are immutable their values cannot be changed
+# once they are created
+
+tuple_a = ("a","b","c","d") # Tuple of string
+tuple_b = (1,2,3,4,5,5) # Tuple of numbers
+tuple_c = (1,"west",32,"flowers") # mixed tuple
+tuple_d = tuple() # empty value
+
+# We can create a tuple using parentheses()
+# as in tuple_a, tuple_b and tuple_c in the above
+# example or we can use the tuple() function
+# as in tuple_d
