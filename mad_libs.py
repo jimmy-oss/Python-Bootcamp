b8f640f4bbd35264df10cfc65cd5423a388666f7
jimmy-oss <jimmynjonge51@gmail.com>
2024-07-12 00:59:26 +0300

    jokebreaker application

--- mad_libs.py
+++ mad_libs.py
@@ -7,31 +7,39 @@
 
 print("Welcome to Jaw_Breaker_Application😂")
 print("Before we proceed please enter your gender: male/female")
-my_gender= input(str()).lower()
+my_gender = input(str()).lower()
 
 if my_gender == "male":
    print("welcome to men's conference😎")
    print("To continue as a man do you support valentines🤔")
    print("Yes😇, or No😈")
+   gender_quiz = input(str()).lower()
 elif my_gender == "female":
-   print("What's up ladies remember what a man can do a women can do let's sing....😂")
-   print("Welcome to Ladies Guys talk night")
+   print("To continue as a lady do you support valentines🤔")
+   print("Yes😇, or No😈")
+   gender_quiz = input(str()).lower()
+
 else:
   print("You are a confused banger")
-man_quiz = input(str()).lower()
-if man_quiz == "yes":
+  
+  
+if gender_quiz == "yes":
   print("Congratulation on your valentines day")
   print("I hope the chocolates you will eat will give you diarrhea")
   print("I hope you will not receive electricity at home ")
   print("I hope your car will breakdown on your way to work")
   print("I hope you will find a Tiger in your backyard")
-elif man_quiz == "no":
+elif gender_quiz == "no":
   print("You are the goat of the day")
   print("You are a freedom fighter cheers to our holiday")
   print("You are the boss we shall drink and watch manchester united all day long")
- 
- 
+elif gender_quiz != "no":
+     print("You are a confused banger")
+elif gender_quiz != "yes":
+  print("You are a confused banger")
+
 else:
   print("You are a confused banger")
+ 
 
  
