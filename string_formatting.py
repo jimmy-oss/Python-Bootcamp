5eb38f8f945ea8c2b12389f8a426e15b7a2d640e
jimmy-oss <jimmynjonge51@gmail.com>
2024-07-11 21:49:57 +0300

    string_formatting

---
+++ string_formatting.py
@@ -0,0 +1,8 @@
+# String Formatting
+# Way of embedding variables into strings
+name = "James"
+age = 19
+print(f"My name is{name} and I am {age} years old")
+# The f before the string stands for f-strings or
+# formatted strings and allow us to put replacement fields
+# {} with variable names inside our strings
